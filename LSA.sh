ccddc48eccb1288723e18de7e54df619f686e438
simp7 <JHParkOfficial@protonmail.com>
2021-07-22 13:42:27 +0900

    Implement LSA to shell script for venv so now it works.

---
+++ LSA.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+source venv/bin/activate
+python3 nlp/LSA.py "$1" "$2"
--- nlp/nlp.go
+++ nlp/nlp.go
@@ -11,7 +11,7 @@ type nlp struct {
 
 func Word2vec() nlp {
 	//TODO: 국내 특허 자연어 처리 명령어 삽입
-	return nlp{"nlp/LSA.py"}
+	return nlp{"./LSA.sh"}
 }
 
 func LDA() nlp {
@@ -21,11 +21,7 @@ func LDA() nlp {
 
 func (n nlp) Process(tmpFile string) ([]byte, error) {
 
-	venv := exec.Command("zsh", "-c", "source "+"./venv/bin/activate")
-
-	err := venv.Run()
-	fmt.Println(err)
-	cmd := exec.Command("python3", n.cmd, tmpFile, "10")
+	cmd := exec.Command(n.cmd, tmpFile, "10")
 
 	result, err := cmd.CombinedOutput()
 	fmt.Println(string(result))
