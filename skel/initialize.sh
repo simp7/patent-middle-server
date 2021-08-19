82d5d08353826e2d03f0f580917e7aaa97db880d
simp7 <JHParkOfficial@protonmail.com>
2021-08-19 14:21:40 +0900

    Add some lines in shell for less pre-requisition and fix errors when installing first time.

--- skel/initialize.sh
+++ skel/initialize.sh
@@ -2,6 +2,8 @@
 
 # shellcheck disable=SC2164
 cd "$HOME"/patent-server/
+sudo apt install python3
+sudo apt install pip3
 sudo apt install python3.8-venv
 python3 -m venv venv
 chmod 744 "$HOME"/patent-server/venv/bin/activate
