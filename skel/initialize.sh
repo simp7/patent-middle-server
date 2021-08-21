c5996d0f5f5206329c7a90597fbb1b29600497f5
JeongHyeon Park <JHParkOfficial@protonmail.com>
2021-08-21 22:54:18 +0900

    Fix Errors from python-version

--- skel/initialize.sh
+++ skel/initialize.sh
@@ -2,11 +2,14 @@
 
 # shellcheck disable=SC2164
 cd "$HOME"/patent-server/
-sudo apt install -y python3
+sudo apt install -y python3.8
 sudo apt install -y python3-pip
-sudo apt install -y python3-venv
-python3 -m venv venv
+sudo apt install -y python3.8-venv
+sudo apt install -y python3.8-dev
+python3.8 -m venv venv
 chmod 744 "$HOME"/patent-server/venv/bin/activate
 source "$HOME"/patent-server/venv/bin/activate
 pip3 install Cython
+pip3 install wheel
+
 pip3 install -r "$HOME"/patent-server/requirements.txt
