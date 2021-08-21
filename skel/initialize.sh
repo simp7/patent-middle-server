35442a62ffd2ffa2408962eb3d37d3a6894d3641
JeongHyeon Park <JHParkOfficial@protonmail.com>
2021-08-21 22:33:22 +0900

    Update initialize.sh

--- skel/initialize.sh
+++ skel/initialize.sh
@@ -3,8 +3,8 @@
 # shellcheck disable=SC2164
 cd "$HOME"/patent-server/
 sudo apt install python3
-sudo apt install pip3
-sudo apt install python3.8-venv
+sudo apt install python3-pip
+sudo apt install python3-venv
 python3 -m venv venv
 chmod 744 "$HOME"/patent-server/venv/bin/activate
 source "$HOME"/patent-server/venv/bin/activate
