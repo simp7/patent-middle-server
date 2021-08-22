66969866f6faa86c6ea138362511989b51eb0b6b
JeongHyeon Park <JHParkOfficial@protonmail.com>
2021-08-22 22:53:38 +0900

    Add jdk for LSA

--- skel/initialize.sh
+++ skel/initialize.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
 # shellcheck disable=SC2164
+
 cd "$HOME"/patent-server/
+
+sudo apt install -y openjdk-8-jdk
 sudo apt install -y python3.8
 sudo apt install -y python3-pip
 sudo apt install -y python3.8-venv
 sudo apt install -y python3.8-dev
+
 python3.8 -m venv venv
 chmod 744 "$HOME"/patent-server/venv/bin/activate
 source "$HOME"/patent-server/venv/bin/activate
+
 pip3 install Cython
 pip3 install wheel
-
 pip3 install -r "$HOME"/patent-server/requirements.txt
